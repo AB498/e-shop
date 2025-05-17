cc9ae16f1db9242e28a42f714d0dc1ca2e45e531
AB498 <abcd49800@gmail.com>
2025-05-17 17:24:54 +0600

    .

--- src/components/ui/ImageWithFallback.jsx
+++ src/components/ui/ImageWithFallback.jsx
@@ -1,10 +1,13 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 
 // Image component with fallback
 const ImageWithFallback = ({ src, fallbackSrc = '/images/product-image.png', alt, ...props }) => {
   const [imgSrc, setImgSrc] = useState(src);
+  useEffect(() => {
+    setImgSrc(src);
+  }, [src]);
 
   return (
     <Image
